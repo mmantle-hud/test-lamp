a7ee381e19740a588c1160c136472ad869c52be5
Matthew Mantle <memantle@googlemail.com>
2020-07-24 13:47:32 +0100

    updated

--- www/list.php
+++ www/list.php
@@ -1,4 +1,7 @@
 <?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 try{
        $conn = new PDO('mysql:host=localhost;dbname=filmsdb', 'root', 'cit2318');
        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
